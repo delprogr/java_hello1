e0eda06fa3c28a403106b0237031103a1ac81a43
Del Progr <delprogr@gmail.com>
2018-02-18 15:17:06 +0200

    First Commit

---
+++ src/git/hello.java
@@ -0,0 +1,11 @@
+package git;
+
+public class hello {
+
+	public static void main(String[] args) {
+		System.out.println("My second Java Program");
+		System.out.println("Java java");
+		System.out.println("Hello!");
+	}
+
+}
